6d2c0d8a6cd56eb57c64a4c39691fbb1de3829e8
praveen <praveensrinu23@outlook.com>
2022-09-29 13:25:40 +0530

    cmt-rg

---
+++ resourcegroup/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "${terraform.workspace}-${var.lob}-${var.project}-${var.zone}-${var.location}-rg"
+  location = var.location
+
+
+  tags = {
+    environment = terraform.workspace
+    project = var.project
+    lob = var.lob
+    zone = var.zone
+  }
+}
+
---
+++ resourcegroup/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+    type = string
+    description = "location where the resource groups needs to be created " 
+}
+
+variable "project" {
+  type = string
+  description = "name of the project"
+}
+
+variable "lob" {
+  type = string
+  description = "name of line of busniess"
+}
+
+variable "zone" {
+  type = string
+  description = "value"
+}
